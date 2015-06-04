fc75d84093923cd39aad63fed1b59b0f8d7dd6be
oizhaolei <oizhaolei@gmail.com>
2015-06-04 14:18:30 +0800

    2015052813

--- app/src/main/java/com/ruptech/chinatalk/MainActivity.java
+++ app/src/main/java/com/ruptech/chinatalk/MainActivity.java
@@ -309,7 +309,6 @@ public class MainActivity extends ActionBarActivity implements
         Intent orgIntent = new Intent(this, OrgActivity.class);
         orgIntent.putExtra(OrgActivity.PARENT_ORG_JID, "100000@" + AppPreferences.IM_SERVER_RESOURCE);
         orgIntent.putExtra(OrgActivity.PARENT_ORG_NAME, getString(R.string.dlmu_title));
-        orgIntent.putExtra(OrgActivity.PARENT_ORG_STUDENT, false);
         startActivity(orgIntent);
     }
 
@@ -318,7 +317,6 @@ public class MainActivity extends ActionBarActivity implements
         Intent orgIntent = new Intent(this, OrgActivity.class);
         orgIntent.putExtra(OrgActivity.PARENT_ORG_JID, "200000@" + AppPreferences.IM_SERVER_RESOURCE);
         orgIntent.putExtra(OrgActivity.PARENT_ORG_NAME, getString(R.string.dlmu_title));
-        orgIntent.putExtra(OrgActivity.PARENT_ORG_STUDENT, false);
         startActivity(orgIntent);
     }
 
