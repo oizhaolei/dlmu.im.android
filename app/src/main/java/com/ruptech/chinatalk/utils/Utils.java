8ebc4c5b3232eb20ac2bfd47e2616513abe0abb7
liujiuyi <296096828@qq.com>
2015-06-05 13:39:08 +0800

    getAppVersionOfClient

--- app/src/main/java/com/ruptech/chinatalk/utils/Utils.java
+++ app/src/main/java/com/ruptech/chinatalk/utils/Utils.java
@@ -538,4 +538,18 @@ public class Utils {
         }
     }
 
+    public static AppVersion getAppVersionOfClient(Context context) {
+        AppVersion ver = new AppVersion();
+        try {
+            PackageInfo packageInfo = context.getPackageManager()
+                    .getPackageInfo(context.getPackageName(), 0);
+            ver.verCode = packageInfo.versionCode;
+            ver.verName = packageInfo.versionName;
+        } catch (NameNotFoundException e) {
+            if (BuildConfig.DEBUG)
+                Log.e(TAG, e.getMessage(), e);
+        }
+
+        return ver;
+    }
 }
